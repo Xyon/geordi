703ff1117871525f447c3e497d29d1a55e40df57
Josh Bladen <josh.bladen@blueprism.com>
2018-12-07 11:13:27 +0000

    Compile options for prelude and _V init helper

--- prelude/orbiter.h
+++ prelude/orbiter.h
@@ -2,3 +2,5 @@ typedef union {
     double data[3];               ///< array data interface
     struct { double x, y, z; };   ///< named data interface
 } VECTOR3;
+
+inline VECTOR3 _V(double x, double y, double z) {     VECTOR3 vec = {x,y,z}; return vec; }
